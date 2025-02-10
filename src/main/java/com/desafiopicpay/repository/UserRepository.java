005a434bf78e5aeaaf9d127887b8c47f85207aba
Esteban Rojas <estebanrojasch.2@gmail.com>
2025-02-10 01:36:07 -0300

    Creacion del repositorio para User

---
+++ src/main/java/com/desafiopicpay/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.desafiopicpay.repository;
+
+import com.desafiopicpay.domain.users.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User>findByDocument(String document);
+}
