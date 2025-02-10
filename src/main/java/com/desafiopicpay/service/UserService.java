d6f53853ce8b61be594e36a920c08960bc915615
Esteban Rojas <estebanrojasch.2@gmail.com>
2025-02-10 17:23:02 -0300

    fix: Agregando anotacion Transacational a metodo.

--- src/main/java/com/desafiopicpay/service/UserService.java
+++ src/main/java/com/desafiopicpay/service/UserService.java
@@ -5,29 +5,39 @@ import com.desafiopicpay.domain.users.User;
 import com.desafiopicpay.mapper.UserDtoToUser;
 import com.desafiopicpay.mapper.UserToUserDto;
 import com.desafiopicpay.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 @Slf4j
+@RequiredArgsConstructor
 public class UserService {
-    @Autowired
-    private UserRepository userRepository;
-    @Autowired
-    private UserDtoToUser mapUserDtoToUser;
-    @Autowired
-    private UserToUserDto mapUserToUserDto;
 
+    private final UserRepository userRepository;
 
-    public void createUserCommon(UserDto userDTO) {
+    private final UserDtoToUser mapUserDtoToUser;
+
+    private final UserToUserDto mapUserToUserDto;
+
+    @Transactional
+    public void createUser(UserDto userDTO) throws Exception {
         log.info("Iniciando proceso de mapeo y creación de usuario.");
         try {
             User user = mapUserDtoToUser.map(userDTO);
             userRepository.save(user);
-        } catch (Exception e) {
+        } catch (DataIntegrityViolationException uniqueValueException) {
+            throw new Exception("A ingresado un valor que no puede ser duplicado. Documento/Email");
+        } catch (HttpMessageNotReadableException valueParserError) {
+            throw new Exception("Ocurrió un error al ingresar uno o mas valores. No coinciden con el tipo de valor requerido.");
+        }catch (Exception e){
             e.printStackTrace();
         }
     }
@@ -41,6 +51,7 @@ public class UserService {
         } catch (Exception e) {
             e.printStackTrace();
         }
-        return null;
+        return new ArrayList<>() {
+        };
     }
 }
