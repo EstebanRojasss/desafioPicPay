aed9c55f51806d6d9374852bdd99836f368ad405
Esteban Rojas <estebanrojasch.2@gmail.com>
2025-02-12 02:06:16 -0300

    feat: Controller y service de la entidad User.

--- src/main/java/com/desafiopicpay/controllers/UserController.java
+++ src/main/java/com/desafiopicpay/controllers/UserController.java
@@ -2,6 +2,7 @@ package com.desafiopicpay.controllers;
 
 import com.desafiopicpay.domain.dtos.UserDto;
 import com.desafiopicpay.service.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,15 +11,16 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/users")
 public class UserController {
 
-    @Autowired
-    private UserService userService;
+
+    private final UserService userService;
 
     @PostMapping
-    public ResponseEntity<Void>createUser(@RequestBody UserDto userDto){
-        userService.createUserCommon(userDto);
+    public ResponseEntity<Void>createUser(@RequestBody UserDto userDto)throws Exception{
+        userService.createUser(userDto);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
--- src/main/java/com/desafiopicpay/service/UserService.java
+++ src/main/java/com/desafiopicpay/service/UserService.java
@@ -9,10 +9,7 @@ import com.desafiopicpay.mapper.UserToUserDto;
 import com.desafiopicpay.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -37,7 +34,7 @@ public class UserService {
         try {
             User user = mapUserDtoToUser.map(userDTO);
             userRepository.save(user);
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
@@ -55,15 +52,20 @@ public class UserService {
         };
     }
 
-    public void authenticateTransaction(UserDto sender, BigDecimal value){
-        if(sender.userType() == UserType.MERCHANT){
-            throw new DoYourExceptions("El ususario " + UserType.MERCHANT +" no puede realizar transferencias.",
+    public void validateTransaction(User sender , BigDecimal value) {
+        if (sender.getUserType() == UserType.MERCHANT) {
+            throw new DoYourExceptions("El ususario " + UserType.MERCHANT + " no puede realizar transferencias.",
                     HttpStatus.FORBIDDEN);
         }
 
-        if(sender.balance().compareTo(value) < 0){
+        if (sender.getBalance().compareTo(value) < 0) {
             throw new DoYourExceptions("El usuario no cuenta con el saldo suficiente para realizar transferencias.",
                     HttpStatus.FORBIDDEN);
         }
+    }
+
+    public User findById(Long id) {
+        return userRepository.findUserById(id)
+                .orElseThrow(() -> new DoYourExceptions("No se encontró el usuario.", HttpStatus.NOT_FOUND));
     }
 }
